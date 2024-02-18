c4109faf2f9ef2ab52817645164aee79d66da577
Svin07 <svin07@ukr.net>
2024-02-18 19:23:38 +0200

    add button and styles in Product page

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -2,18 +2,16 @@
 
 <script setup>
 import { fetchSneakersbyId } from '@/API/api'
-import { onMounted, ref } from 'vue'
+import { inject, onMounted, ref } from 'vue'
 
 import { useRoute } from 'vue-router'
 
-// import { computed } from 'vue'
+const onClickAdd = inject('onClickAdd')
 
 const route = useRoute()
 const productId = route.params.id
 
 const product = ref({})
-
-// const productId = this.$route.params
 
 onMounted(async () => {
   product.value = await fetchSneakersbyId(productId)
@@ -21,27 +19,45 @@ onMounted(async () => {
 </script>
 
 <template>
-  <button class="bg-lime-500 p-1 rounded-md hover:bg-lime-600 mb-4">Go back</button>
-  <div class="flex flex-col mb-10">
-    <div class="flex justify-between items-center flex-nowrap gap-8 mb-4">
-      <img class="flex-1 w-1/3" :src="product.imageUrl" :alt="product.title" />
-      <div class="flex flex-col gap-8">
-        <h2 class="text-xl font-semibold">{{ product.title }}</h2>
-        <h3>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex ut optio explicabo
-          temporibus necessitatibus suscipit saepe ea error? Ipsam natus doloribus at obcaecati
-          beatae ducimus consequuntur atque quam ut. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Odio ex ut optio explicabo temporibus necessitatibus suscipit saepe ea
-          error? Ipsam natus doloribus at obcaecati beatae ducimus consequuntur atque quam ut.
-        </h3>
-        <p class="text-lg"><span class="font-semibold">Ціна:</span> {{ product.price }} грн</p>
-      </div>
-    </div>
+  <router-link to="/"
+    ><button class="bg-lime-500 p-1 rounded-md hover:bg-lime-600 mb-4">Go back</button></router-link
+  >
 
-    <button
-      class="transition bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-300 hover:bg-lime-600 active:bg-lime-700 cursor-pointer"
-    >
-      Додати до кошика
-    </button>
+  <div class="grid grid-cols-3 gap-8 mb-4">
+    <img class="" :src="product.imageUrl" :alt="product.title" />
+    <div class="flex flex-col gap-8">
+      <h2 class="text-xl font-semibold text-center">{{ product.title }}</h2>
+      <h3 class="max-w-68 text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex ut optio explicabo
+        temporibus necessitatibus suscipit saepe ea error? Ipsam natus doloribus at obcaecati beatae
+        ducimus consequuntur atque quam ut. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Odio ex ut optio explicabo temporibus necessitatibus suscipit saepe ea error? Ipsam natus
+        doloribus at obcaecati beatae ducimus consequuntur atque quam ut.
+      </h3>
+      <p class="text-lg"><span class="font-semibold">Ціна:</span> {{ product.price }} грн</p>
+      <button
+        @click="
+          () => {
+            onClickAdd(product)
+          }
+        "
+        class="transition bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-300 hover:bg-lime-600 active:bg-lime-700 cursor-pointer"
+      >
+        Додати до кошика
+      </button>
+    </div>
+    <div class="w-50">
+      <h3 class="block text-xl font-semibold">Оплата/Доставка</h3>
+      <h4 class="text-lg font-semibold mt-4 mb-2">Доставка</h4>
+      <ul class="text-sm list-disc ml-4">
+        <li>по Україні "Нова пошта"</li>
+        <li>по Україні "Укрпошта"</li>
+      </ul>
+      <h4 class="text-lg font-semibold mt-4 mb-2">Оплата</h4>
+      <ul class="text-sm list-disc ml-4">
+        <li>Банківський переказ</li>
+        <li>Електронний гаманець</li>
+      </ul>
+    </div>
   </div>
 </template>
